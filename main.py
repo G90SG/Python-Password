7c3b889f9f8b7b82b838e5c0246f1e3aa7f1d0be
Gemma Dingwall <gemma9261@hotmail.com>
2020-11-02 11:16:28 +0000

    file

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
-#first draft saved#
-#Creating Yes and no list
-yesList = ["Yes", "Y", "y", "Yeah", "yes"]
-noList = ["No", "N", "n", "Nah", "no"]
+#Create a program that asks the user to input a password, re-enter the password
+#and then tells the user if the password is weak, medium or strong.
+passone = input("Enter a password here: ")
 
-#Asking the user to create and enter a suitable password
-print ("Please choose a suitable password.")
-passone = input ("Enter your chosen password: ") 
-
-#Creating a function to check whether the password re-entered matches the first one
-def password():
-  passtwo = input ("To confirm, type the password again: ") 
-
-# Checking whether passwords match
-  if passone == passtwo:
-    print("Great, your passwords match.")
-    validation(passone)
-  else:
-    print("Your passwords don't match.")
-    again = input("Would you like to try again? ")
-    if again in yesList:
-      password()
-    elif again in noList:
-      print ("Goodbye. ")
+weak = "This password is weak, consider adding some extra characters, upper letters or numbers!"
+medium = "This password is medium, consider adding some numbers to make it stronger!"
+strong = "This password is is considered strong!"
+#Make sure the password is ok
+if len(passone) > 12:
+    print("Your password must be longer than 5 and below 12!")
+elif len(passone) < 6:
+    print("Your password must longer than 5 and below 12!")
+lowercase_found = 0
+uppercase_found = 0
+numeric_found = 0
+#Set flags where appropriate
+for ch in passone:
+    if ch.islower():
+        lowercase_found = 1
+    if ch.isupper():
+        uppercase_found = 1
+    if ch.isdigit():
+        numeric_found = 1
+#Exit if all flags have been set
+    if lowercase_found and uppercase_found and numeric_found:
+        break
+#Calculate password strength and print results
+password_strength = lowercase_found + uppercase_found + numeric_found
+#If password meets all three critertias
+if password_strength in range (1,4):
+    print("The password", passone, "is valid.")
+#Print out password strength
+    if password_strength == 1:
+        print("Password", passone, "is a WEAK password!")
+    elif password_strength == 2:
+        print("Password", passone, "is a MEDIUM password!")
     else:
-      print ("That's not the answer I was looking for, Goodbye. ")
-
-def validation(passone):
-
-  
-
-password()
-validation(passone)
+        print("Password", passone, "is a STRONG password!")
+else:
+    print("Invalid password: ", passone, "contained neither letters or numbers!")
